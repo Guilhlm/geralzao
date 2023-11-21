de4d2d2327f12fe63908b065e0d25419daf60892
Guilherme Maia <141831938+Guilhlm@users.noreply.github.com>
2023-11-21 11:59:23 -0300

    alguns comandos

---
+++ teste de tudo/teste de tudo/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace teste_de_tudo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "calcular1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(217, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "celsius";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(217, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "kelvin";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "resposta";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(296, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "escrito";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(296, 48);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "caixadetexto";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "continente americano",
+            "continente africano",
+            "continente asiatico",
+            "continente europeu",
+            "continente oceania",
+            "continente antartico"});
+            this.comboBox1.Location = new System.Drawing.Point(34, 285);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "em qual continente estamos ?";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(58, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 52);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "calcular2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "macarrao",
+            "strogonoff",
+            "peixe",
+            "arroz",
+            "frango"});
+            this.checkedListBox1.Location = new System.Drawing.Point(608, 25);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(157, 79);
+            this.checkedListBox1.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(647, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 51);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "calcular3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(627, 367);
+            this.maskedTextBox1.Mask = "(99) 00000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(138, 20);
+            this.maskedTextBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ teste de tudo/teste de tudo/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste_de_tudo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double numero = double.Parse(textBox1.Text);
+            bool temdescricao = checkBox1.Checked;
+            string descricao = " ", menssagem = " ";
+            bool comalerta = checkBox2.Checked;
+
+            if (radioButton1.Checked)
+            {
+                double fahrenheit = 0;
+
+                fahrenheit = (numero * 1.8) + 32;
+
+                if (temdescricao)
+                {
+                    descricao = "farenheit";
+                }
+               
+                menssagem = fahrenheit.ToString() + descricao;
+
+                if (comalerta)
+                {
+                    MessageBox.Show(menssagem + "ferenheit");
+                }
+                else
+                {
+                    label1.Text = menssagem;
+                }
+            }
+
+
+            if (radioButton2.Checked)
+            {
+                double kelvin = 0;
+
+                kelvin = (numero + 273);
+
+                if (temdescricao)
+                {
+                    descricao = "kelvin";
+                }
+
+                menssagem = kelvin.ToString() + descricao;
+
+                if (comalerta)
+                {
+                    MessageBox.Show(menssagem + "kelvin");
+                }
+                else
+                {
+                    label1.Text = menssagem;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string continente = "";
+
+            continente = comboBox1.SelectedItem.ToString();
+
+            MessageBox.Show(continente);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int contador = 0;
+
+            foreach (string elementos in checkedListBox1.CheckedItems)
+            {
+                MessageBox.Show(elementos);
+
+                contador++;
+            }
+
+
+            MessageBox.Show("foram selecionados " + contador.ToString() + "checkbox");
+
+        }
+    }
+}
